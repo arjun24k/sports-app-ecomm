c1a71179c4a811b690e3aad7315976501a2adaa3
Arjun MK <arjunmkp18@gmail.com>
2021-01-19 14:20:56 +0530

    final-commit-2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000 || 3000;
 const app = express();
 app.use(express.json());
 const {initDb} = require('./initDb');
